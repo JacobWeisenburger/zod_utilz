deb9888ccd4bb894974742cdb002223566ebbd32
Jacob Weisenburger <31667350+JacobWeisenburger@users.noreply.github.com>
2023-01-14 10:41:53 -0600

    Removed getErrorMessage

--- src/getErrorMessage.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod'
-import { safeParseResult } from "./safeParseResult.ts"
-
-export function getErrorMessage ( result: z.SafeParseReturnType<unknown, unknown> ) {
-    return safeParseResult( result ).error?.issues[ 0 ].message
-}
