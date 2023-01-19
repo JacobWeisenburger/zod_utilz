207080654a0a4eec091f12a7c7f6ace9b087fc58
Jacob Weisenburger <31667350+JacobWeisenburger@users.noreply.github.com>
2023-01-19 11:01:30 -0600

    0.1.7

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -10,7 +10,7 @@ await build( {
     shims: { deno: true },
     package: {
         name: 'zod_utilz',
-        version: '0.1.6',
+        version: '0.1.7',
         author: 'JacobWeisenburger',
         description: 'Framework agnostic utilities for Zod',
         license: 'MIT',
